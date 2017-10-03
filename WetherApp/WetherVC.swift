4bd17645f9e1b6e517e33250e42fa0ba5c8fc600
RANDOM <mohamedrafat@iMac.local>
2017-10-03 06:40:07 +0200

    App Design

---
+++ WetherApp/WetherVC.swift
@@ -0,0 +1,20 @@
+//
+//  MainVC.swift
+//  WetherApp
+//
+//  Created by RANDOM on 10/1/17.
+//  Copyright © 2017 RANDOM. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
