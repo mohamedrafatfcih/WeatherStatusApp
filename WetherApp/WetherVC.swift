a1c93bfee6919e07e22842c5beccdb205f3863fb
RANDOM <mohamedrafat@iMac.local>
2017-10-03 06:47:59 +0200

    App Design is Done

--- WetherApp/WetherVC.swift
+++ WetherApp/WetherVC.swift
@@ -9,12 +9,44 @@
 import UIKit
 import Alamofire
 
-class MainVC: UIViewController {
+class WetherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var currentWetherImage: UIImageView!
+    
+    @IBOutlet weak var currentWetherType: UILabel!
 
+    @IBOutlet weak var daysDegreeTblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        daysDegreeTblView.delegate = self
+        daysDegreeTblView.dataSource = self
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = daysDegreeTblView.dequeueReusableCell(withIdentifier: "WetherCell", for: indexPath)
+        return cell
+    }
 }
+
+
+
+
+
+
+
+
+
 
